4f98320607bd0d7b96dd4d7fc1b7ddf403602502
Alberto Fuentes <albertofuentes505@outlook.com>
2021-12-09 20:13:35 -0600

    added new rules

--- config.def.h
+++ config.def.h
@@ -27,9 +27,13 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class	    instance    title       tags mask     isfloating   monitor */
+	{ "Gimp",	    NULL,       NULL,       0,            1,           -1 },
+	{ "Firefox",	    NULL,       NULL,       1 << 1,       0,           -1 },
+	{ "Thunderbird",    NULL,       NULL,       1 << 4,       0,           -1 },
+	{ "Pavucontrol",    NULL,       NULL,       1 << 8,       1,           -1 },
+	{ "PacketTracer",   NULL,       NULL,       1 << 8,       1,           -1 },
+	{ "sxiv",	    NULL,       NULL,       1 << 8,       1,           -1 },
 };
 
 /* layout(s) */
